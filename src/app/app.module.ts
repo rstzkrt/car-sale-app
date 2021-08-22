2b322b7ca51332fd0dd57c6997ee4eb0396d3324
Resat Ozkurt <ozkurtresat@gmail.com>
2021-08-22 13:11:04 +0200

    add advertList component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AdvertListComponent } from './components/advert-list/advert-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdvertListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/advert-list/advert-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdvertListComponent } from './advert-list.component';
+
+describe('AdvertListComponent', () => {
+  let component: AdvertListComponent;
+  let fixture: ComponentFixture<AdvertListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AdvertListComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdvertListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
