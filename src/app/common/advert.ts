6a46aaa5c604d5ff4ca8293bf203ebcdf29a207e
Resat Ozkurt <ozkurtresat@gmail.com>
2021-08-22 13:32:07 +0200

    implement advert class

---
+++ src/app/common/advert.ts
@@ -0,0 +1,13 @@
+export class advert {
+
+  constructor( description: string,
+  title: string,
+  // postedBy: AppUser,
+  price:number,
+  // images:Image[],
+  address:string,
+  //car:Car,
+  //reports:Report[]
+  postDate:Date) {}
+
+}
