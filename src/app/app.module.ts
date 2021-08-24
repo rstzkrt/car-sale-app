1d8d7edf0afec7151e6ad0a9922a03a69995085e
Resat Ozkurt <ozkurtresat@gmail.com>
2021-08-24 10:11:48 +0200

    add angular-material cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
+import { MatCardModule } from "@angular/material/card";
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AdvertListComponent } from './components/advert-list/advert-list.component';
 import {HttpClientModule} from "@angular/common/http";
 import {AdvertService} from "./services/advert.service";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+
 
 @NgModule({
   declarations: [
@@ -13,9 +17,12 @@ import {AdvertService} from "./services/advert.service";
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [AdvertService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
