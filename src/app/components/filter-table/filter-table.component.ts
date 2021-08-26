9d83ff0547041db0c7e01b30d8a5735312aaad54
Resat Ozkurt <ozkurtresat@gmail.com>
2021-08-26 23:40:16 +0200

    add filter table component

---
+++ src/app/components/filter-table/filter-table.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-table',
+  templateUrl: './filter-table.component.html',
+  styleUrls: ['./filter-table.component.css']
+})
+export class FilterTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  formatLabel(value: number) {
+    return value + '$';
+  }
+
+}
