5a315fb492fffab30aa32d5b04d2e3c995b6aece
Resat Ozkurt <ozkurtresat@gmail.com>
2021-08-26 17:49:08 +0200

    Advert image sizes modified

--- src/app/components/advert-list/advert-list.component.ts
+++ src/app/components/advert-list/advert-list.component.ts
@@ -10,16 +10,11 @@ import {Advert} from "../../common/advert";
 export class AdvertListComponent implements OnInit {
 
   adverts: Advert[];
-  gridColumns = 3;
 
   constructor(private advertService: AdvertService) {}
 
   ngOnInit(): void {
     this.listAdverts();
-  }
-
-  toggleGridColumns() {
-    this.gridColumns = this.gridColumns === 3 ? 4 : 3;
   }
 
   listAdverts() {
