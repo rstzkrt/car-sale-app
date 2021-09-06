37ebcebae15b0826f2415b5fd31b40776269cd3e
resat <ozkurtresat@gmail.com>
2021-09-06 16:20:55 +0200

    add User model

--- src/app/common/user.ts
+++ src/app/common/user.ts
@@ -1,2 +1,13 @@
+import {Advert} from "./advert";
+
 export class User {
+  id:string;
+  firstName:string;
+  lastName:string;
+  email:string;
+  role:string;
+  avatar:string;
+  dateOfBirth:Date;
+  myAdverts:Advert[];
+  favourites:Map<string,boolean>;
 }
