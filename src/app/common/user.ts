2cad915ac92bb21592d67dc70d2a6b573461dc52
resat <ozkurtresat@gmail.com>
2021-09-08 23:30:00 +0200

    update user entity
    add uID field

--- src/app/common/user.ts
+++ src/app/common/user.ts
@@ -1,13 +1,11 @@
-import {Advert} from "./advert";
-
 export class User {
   id:string;
+//  uId:string;
   firstName:string;
   lastName:string;
   email:string;
   role:string;
   avatar:string;
   dateOfBirth:Date;
-  myAdverts:Advert[];
   favourites:Map<string,boolean>;
 }
