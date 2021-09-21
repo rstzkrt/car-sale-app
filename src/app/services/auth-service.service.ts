cfb5e2114abbcce6d3c422ec5e1b41a805e013b0
resat <ozkurtresat@gmail.com>
2021-09-21 10:43:09 +0200

    implement login with google method

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {Router} from "@angular/router";
+import firebase from "firebase/compat/app";
+import 'firebase/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
 
-  constructor() { }
+  user:firebase.User;
+  constructor(private afAuth: AngularFireAuth, private router: Router) {
+    this.afAuth.authState.subscribe(user=>{
+      this.user=user;
+    });
+  }
+
+  async loginWithGoogle() {
+   await this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(
+     res =>{
+       console.log("Login Success")
+       //uid, email, avatar
+       //
+       //res.additionalUserInfo.isNewUser ise back end ten mongoya user ekle ... ile
+     }
+   ).catch(err=>{
+     console.log("Login Err")
+   })
+  }
 }
