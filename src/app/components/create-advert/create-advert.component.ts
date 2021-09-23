a93728eb880f708db4da99d8d66cd8b7ddf1930a
resat <ozkurtresat@gmail.com>
2021-09-23 17:01:48 +0200

    add Form for creating advert

--- src/app/components/create-advert/create-advert.component.ts
+++ src/app/components/create-advert/create-advert.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {AdvertService} from "../../services/advert.service";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {Advert} from "../../common/advert";
+import {User} from "../../common/user";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {AuthServiceService} from "../../services/auth-service.service";
+import {UserService} from "../../services/user.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-create-advert',
@@ -7,9 +15,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAdvertComponent implements OnInit {
 
-  constructor() { }
+  checkoutFormGroup:FormGroup;
+
+  constructor(private userService:UserService,
+              private advertService:AdvertService,
+              private authService:AuthServiceService,
+              private afAuth:AngularFireAuth,
+              private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.checkoutFormGroup=this.formBuilder.group({
+      advert:this.formBuilder.group({
+        description: [''],
+        title: [''],
+        price:[''],
+        city: [''],
+        coverPhoto:[''],
+        postedBy: [''],
+        carBrand:[''],
+        carTransmission:[''],
+        carMileage:[''],
+        carBodyType:[''],
+        carFuelType:[''],
+        carCondition:[''],
+        photos:['']
+      })
+    })
   }
 
+
+  createAdvert() : Observable<any> {
+    let advert = this.checkoutFormGroup.get('advert').value;
+    advert.postedBy = "614ba7f3dff5b07bcd8ab200";
+    console.log(advert)
+
+    return this.advertService.createAdvert(advert);
+  }
+
+  // createAdvert(advert:Advert):Promise<any>{
+  //
+  //   this.checkoutFormGroup.get('advert').value.postedBy = "614ba7f3dff5b07bcd8ab200";
+  //   console.log(advert)
+  //   return this.advertService.createAdvert(advert);
+  //
+  // }
+  //
+  // async OnSubmit() {
+  //   await this.createAdvert(this.checkoutFormGroup.get('advert').value);
+  //
+  // }
 }
