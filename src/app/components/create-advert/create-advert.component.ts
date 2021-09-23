2d19d61f113afd55f93787e2b135fec8726c8c31
resat <ozkurtresat@gmail.com>
2021-09-23 16:55:17 +0200

    add create-advert component

---
+++ src/app/components/create-advert/create-advert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-advert',
+  templateUrl: './create-advert.component.html',
+  styleUrls: ['./create-advert.component.css']
+})
+export class CreateAdvertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
