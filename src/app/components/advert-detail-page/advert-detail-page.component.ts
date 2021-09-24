0a568800d4f4bb1c9eb94e8c571bb318ac3e75b0
resat <ozkurtresat@gmail.com>
2021-09-24 13:20:29 +0200

    add update and delete advert buttons to detail page

--- src/app/components/advert-detail-page/advert-detail-page.component.ts
+++ src/app/components/advert-detail-page/advert-detail-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AdvertService} from "../../services/advert.service";
 import {Advert} from "../../common/advert";
 import {ActivatedRoute} from "@angular/router";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
 
 @Component({
   selector: 'app-advert-detail-page',
@@ -12,12 +13,22 @@ export class AdvertDetailPageComponent implements OnInit {
 
   advert:Advert;
 
-  constructor(private advertService:AdvertService, private route:ActivatedRoute) { }
+  constructor(private advertService:AdvertService,
+              private route:ActivatedRoute,
+              public afAuth:AngularFireAuth) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(()=>{
       this.handleAdvertDetail();
     })
+  }
+
+  onDelete(){
+    //
+  }
+
+  onUpdate(){
+    //
   }
 
   handleAdvertDetail(){
