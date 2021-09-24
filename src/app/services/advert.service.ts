e1e4e717fd76c6e2d6695ea28e540dcf775285bd
resat <ozkurtresat@gmail.com>
2021-09-24 17:02:43 +0200

    add deleteAdvert method

--- src/app/services/advert.service.ts
+++ src/app/services/advert.service.ts
@@ -38,29 +38,33 @@ export class AdvertService {
     return this.httpClient.get<Advert[]>(url);
   }
 
-  createAdvert(advert:Advert):Observable<any> {
-const token="eyJhbGciOiJSUzI1NiIsImtpZCI6ImFlNTJiOGQ4NTk4N2U1OWRjYWM2MmJlNzg2YzcwZTAyMDcxN2I0MTEiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiRnVubnlNb21lbnRzIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hLS9BT2gxNEdpSlRXWmxHSW1tTWs5eHRDazh6N3RmMjZQYzlSdjZCNERRd0NncD1zOTYtYyIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9jYXItc2FsZS1hdXRoIiwiYXVkIjoiY2FyLXNhbGUtYXV0aCIsImF1dGhfdGltZSI6MTYzMjQwNDc5NiwidXNlcl9pZCI6IjVWYWxPSTAxQXBXazdMMVZKcVk0TmgwckF4OTMiLCJzdWIiOiI1VmFsT0kwMUFwV2s3TDFWSnFZNE5oMHJBeDkzIiwiaWF0IjoxNjMyNDA3NDQzLCJleHAiOjE2MzI0MTEwNDMsImVtYWlsIjoidGhlY2F2YWxyeWR3QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7Imdvb2dsZS5jb20iOlsiMTEwMjEzODk3MDYxNzYyOTQwNjI0Il0sImVtYWlsIjpbInRoZWNhdmFscnlkd0BnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJnb29nbGUuY29tIn19.cxKAF1LX74XqERmaL9JtGYsslmKc4y4PQOFSFmj3hAa86VZwUYN86Nbbgt5twFXI5SvmZ_OJz_uPnQ4o77A7oVNc8mOzuLpLpZflOcL_YuHFfK__2-64-yZulva7yFIij0lY4vRXq5PnvXf2TT6hRyBq2PM2TBJsvFPT7g1yUt6bDKn9jJkpoIeSRgkU95WKanttT7k-EmnkTbqzbQa9N-nrP0fsZ_1gG5GNUV1J7imW_-qqeoKReP0UMTuklNWcFOUZIEmWdsrBXeV_sC0Z6KTeRgLYWlZNgQfTQmNipnNTKiOlk8XO25Lm90gyOCAd7_dr2-ygf-5h_Zsn2TLOIg";
+  createAdvert(advert: Advert, token: string):Observable<any> {
+    const headers = { 'content-type': 'application/json','Authorization': `Bearer ${token}`};
 
-    // this.getToken().then(token => {
-      const headers = { 'content-type': 'application/json','Authorization': `Bearer ${token}`};
-
-     return this.httpClient.post(this.baseUrl, {
-        postedBy: advert.postedBy,
-        description: advert.description,
-        title : advert.description,
-        price: advert.price,
-        city: advert.city,
-        coverPhoto: advert.coverPhoto,
-        carBrand: advert.carBrand,
-        carTransmission: advert.carTransmission,
-        carMileage: advert.carMileage,
-        carBodyType: advert.carBodyType,
-        carFuelType: advert.carFuelType,
-        carCondition: advert.carCondition,
-        photos: []
-      },{'headers':headers});
-    // })
+    return this.httpClient.post(this.baseUrl, {
+      postedBy: advert.postedBy,
+      description: advert.description,
+      title : advert.description,
+      price: advert.price,
+      city: advert.city,
+      coverPhoto: advert.coverPhoto,
+      carBrand: advert.carBrand,
+      carTransmission: advert.carTransmission,
+      carMileage: advert.carMileage,
+      carBodyType: advert.carBodyType,
+      carFuelType: advert.carFuelType,
+      carCondition: advert.carCondition,
+      photos: []
+    },{'headers':headers});
   }
+
+  deleteAdvert(advertId:string,token:string):Observable<any>{
+    const url=`http://localhost:8080/adverts/${advertId}`
+    const headers = { 'content-type': 'application/json','Authorization': `Bearer ${token}`};
+    return this.httpClient.delete(url,{'headers':headers})
+  }
+
+
 }
 
 
