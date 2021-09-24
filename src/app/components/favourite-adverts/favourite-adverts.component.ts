5326504f0414067218aa0fec77852f37eebfa58c
resat <ozkurtresat@gmail.com>
2021-09-24 13:17:34 +0200

    add favourite-adverts components

---
+++ src/app/components/favourite-adverts/favourite-adverts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-favourite-adverts',
+  templateUrl: './favourite-adverts.component.html',
+  styleUrls: ['./favourite-adverts.component.css']
+})
+export class FavouriteAdvertsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
