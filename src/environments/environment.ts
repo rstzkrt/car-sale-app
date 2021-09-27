04e9eca68d80097c66fe085cd07c0325a94607bd
resat <ozkurtresat@gmail.com>
2021-09-27 18:54:59 +0200

    add express to deploy heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
+    apiKey: "",
+    authDomain: "car-sale-auth.firebaseapp.com",
+    projectId: "car-sale-auth",
+    storageBucket: "car-sale-auth.appspot.com",
+    messagingSenderId: "1030360887342",
+    appId: "1:1030360887342:web:4be4f20782aeb7bd4e2fbf"
   }
 };
 
